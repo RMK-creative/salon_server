4a6451c67ef0592042dd2e7d950cb5d1f21ff2ed
Hanna Mazur <hanna2mazur@gmail.com>
2022-03-30 16:04:53 +0200

    Auth routers added

--- app.js
+++ app.js
@@ -2,26 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = process.env.PORT || 3080;
+const authRouters = require('./routers/authRouters');
+const cookieParser = require('cookie-parser');
 
 const Customer = require("./models/customer");
 
 const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
 require("dotenv").config();
 
 const mongoDB = process.env.DB_URI;
 mongoose.connect(mongoDB);
 
-app.post("/", (req, res) => {
-  Customer.create({
-    name: "Szanzinski",
-    first_name: "Rosie",
-    email: "rosie@mail.com",
-  }).then(function (newCustomer) {
-    console.log("new customer added");
-    res.send(newCustomer);
-  });
-});
+app.use(authRouters);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
+
 
 const userSchema = new Schema({
     name: {
@@ -7,18 +10,48 @@ const userSchema = new Schema({
         type: String
     },
     email: {
-        required: true,
-        type: String
-    },
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+  },
     password: {
-        required: true,
-        type: String
-    },
+        type: String,
+        required: [true, 'Plese enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters'],
+  },
     role: {
         required: true,
-        type: String
+        type: String,
+        role: ['customer', 'admin', 'staff']
     }
 });
+//fire a function after doc saved to db
+userSchema.post('save', function(doc, next) {
+    console.log('new user was created', doc);
+    next()
+})
+
+//fire a function befor doc saved to db
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return user;
+      }
+      throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+  };
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
