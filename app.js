74051bf62bcfdd7db9751bffbf24801bcdde1afa
Hanna Mazur <hanna2mazur@gmail.com>
2022-04-05 14:12:58 +0200

    add cors origin

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  req.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
   next();
 });
 // body parser
